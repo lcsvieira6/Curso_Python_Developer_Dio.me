f4797e759cdba246eb42ee4c1d9158d4cd8f32ed
Lucas Vieira <lcsvieira6@gmail.com>
2023-07-28 10:51:07 -0300

    Atualização no nome das pastas e inclusão de nova pasta

---
+++ 1 - Fundamentos_de_Python/4.1 - conversão_de_tipos.py
@@ -0,0 +1,19 @@
+idade = "28"
+print(idade)
+print(type(idade))
+
+'''
+Convertendo para Integer
+'''
+
+idade = int(idade)
+print(idade)
+print(type(idade))
+
+'''
+Convertendo para Float
+'''
+
+idade = float(idade)
+print(idade)
+print(type(idade))
